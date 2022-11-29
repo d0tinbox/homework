b3fa16a7894c4cde2590cb68e219b3eddbafdb1f
AND <catsAND@users.noreply.github.com>
2022-11-29 17:11:00 +0200

    fix: add -c argument to switch logger output to console

--- Logger.php
+++ Logger.php
@@ -6,6 +6,7 @@ require 'vendor/autoload.php';
 
 use Homework\Logger\AbstractHandler;
 use Homework\Logger\HandlerFactory;
+use Homework\Logger\Type;
 
 class Logger
 {
@@ -14,9 +15,25 @@ class Logger
     public static function get(): AbstractHandler
     {
         if (self::$instance == null) {
-            self::$instance = HandlerFactory::get();
+            self::$instance = HandlerFactory::get(self::getHandlerType());
         }
 
         return self::$instance;
+    }
+
+    /**
+     * Get handler type.
+     *
+     * @return Type
+     */
+    private static function getHandlerType(): Type
+    {
+        if (count($_SERVER['argv']) > 1) {
+            if ($_SERVER['argv'][1] === '-c' || $_SERVER['argv'][1] === '--console') {
+                return Type::CONSOLE;
+            }
+        }
+
+        return Type::FILE;
     }
 }
